a63fbcb1eab8dcc11ff78a6e6e5b043df3d9104a
Clément Dreptin <cdreptin@gmail.com>
2023-03-27 00:26:34 +0200

    feat(nvim): used the default lualine options

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,10 @@
 return {
   'nvim-lualine/lualine.nvim',
-  opts = {
-    options = {
-      icons_enabled = false,
-      theme = 'onedark',
-      component_separators = '|',
-      section_separators = '',
-    },
-  },
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'onedark',
+      },
+    })
+  end,
 }
