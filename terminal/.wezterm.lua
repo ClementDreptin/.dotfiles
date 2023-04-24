edca3e3c3650e6e410638eda066355267a414b33
Clément Dreptin <cdreptin@gmail.com>
2023-04-24 14:35:39 +0200

    feat(terminal): added the previous keybindings to the wezterm config

--- terminal/.wezterm.lua
+++ terminal/.wezterm.lua
@@ -15,4 +15,48 @@ wezterm.on('gui-startup', function(cmd)
   window:gui_window():maximize()
 end)
 
+-- Keybindings
+config.keys = {
+  -- Split pane
+  {
+    key = 'e',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal,
+  },
+  {
+    key = 'o',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical,
+  },
+
+  -- New tab
+  {
+    key = 't',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SpawnTab('CurrentPaneDomain'),
+  },
+
+  -- Resize pane
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.AdjustPaneSize({ 'Left', 2 }),
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.AdjustPaneSize({ 'Right', 2 }),
+  },
+  {
+    key = 'UpArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.AdjustPaneSize({ 'Up', 1 }),
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.AdjustPaneSize({ 'Down', 1 }),
+  },
+}
+
 return config
