dfcb3a4da794c4c4c44e2f5923e42e51ca9350b4
Clément Dreptin <cdreptin@gmail.com>
2023-06-09 16:32:45 +0200

    feat(js): added prettier to the eslint configs and removed the rules that conflict with it

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -1,12 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: 'standard',
+  extends: ["standard", "prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    semi: ['error', 'always'],
-    'no-extra-semi': 'error',
-    'comma-dangle': ['error', 'always-multiline'],
+    ecmaVersion: "latest",
   },
 };
