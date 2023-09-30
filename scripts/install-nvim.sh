63f7bad05b0f1746836751eecb3b65fda33a23fb
Clément Dreptin <cdreptin@gmail.com>
2023-09-30 13:48:41 +0200

    feat(scripts): added a script to install Neovim and its dependencies

---
+++ scripts/install-nvim.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+GIT_DIR="$(git rev-parse --show-toplevel)"
+USER_BIN_DIR=$HOME/.local/bin
+USER_CFG=$HOME/.config
+NVIM_BIN=$USER_BIN_DIR/nvim
+NVIM_CFG=$USER_CFG/nvim
+
+# Create the user's private bin directory if needed
+if [ ! -d $USER_BIN_DIR ]; then
+  echo "Private bin directory ($USER_BIN_DIR) not found, creating..."
+  mkdir -p $USER_BIN_DIR
+fi
+
+# Install the Neovim binary if needed
+if [ ! -f $NVIM_BIN ]; then
+  # Install libfuse2 if needed (required to run AppImages)
+  dpkg -s libfuse2 >/dev/null 2>&1
+  if [ ! $? -eq 0 ]; then
+    echo "libfuse2 is required to run AppImages and was not found, installing..."
+    sudo apt install libfuse2
+  fi
+
+  # Download Neovim binary from GitHub and make it executable
+  echo "Neovim binary not found, installing..."
+  curl -Lo $NVIM_BIN https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  chmod u+x $NVIM_BIN
+fi
+
+# Install Neovim config if needed
+if [ ! -d $NVIM_CFG ]; then
+  echo "Installing Neovim config..."
+
+  # Create the user config directory if needed
+  if [ ! -d $USER_CFG ]; then
+    mkdir -p $USER_CFG
+  fi
+
+  ln -s $GIT_DIR/nvim $NVIM_CFG
+fi
+
+# Install the Neovim plugins' system dependencies if needed
+deps=(ripgrep build-essential zip)
+for dep in ${deps[@]}; do
+  dpkg -s $dep >/dev/null 2>&1
+  if [ ! $? -eq 0 ]; then
+    echo "$dep is required and was not found, installing..."
+    sudo apt install $dep
+  fi
+done
+
+# Install lazygit if needed
+lazygit --version >/dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+  echo "lazygit is not installed, installing..."
+
+  lazygit_version=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  lazygit_tar=lazygit.tar.gz
+  curl -Lo $lazygit_tar "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${lazygit_version}_Linux_x86_64.tar.gz"
+  tar xf $lazygit_tar lazygit
+  rm $lazygit_tar
+  mv lazygit $USER_BIN_DIR/lazygit
+fi
