985c8fa356f28c31fec7f7e7181568ffe91aba0c
Clément Dreptin <cdreptin@gmail.com>
2024-06-28 11:16:56 +0200

    feat(nvim): added an lsp config for rust

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,6 +7,13 @@ local servers = {
   },
   tsserver = {},
   jsonls = {},
+  rust_analyzer = {
+    ["rust-analyzer"] = {
+      checkOnSave = {
+        command = "clippy",
+      },
+    },
+  },
 }
 
 -- All the config needed to make LSPs talk with the LSP client in Neovim
