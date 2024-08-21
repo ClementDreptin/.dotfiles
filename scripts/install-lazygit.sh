42d30587ffc62cf69272400c87449ae2ba9f20bd
Clément Dreptin <cdreptin@gmail.com>
2024-08-21 09:19:40 +0200

    docs(scripts): added comment in install-lazygit.sh

--- scripts/install-lazygit.sh
+++ scripts/install-lazygit.sh
@@ -12,6 +12,7 @@ fi
 
 echo "Installing lazygit..."
 
+# Download the latest lazygit binary from GitHub and put it in the user's private bin directory
 lazygit_version=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 lazygit_tar=lazygit.tar.gz
 curl -Lo $lazygit_tar "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${lazygit_version}_Linux_x86_64.tar.gz"
