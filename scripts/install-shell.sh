3056fa072c048dce4e0c97cf45832030d02f5c20
Clément Dreptin <cdreptin@gmail.com>
2024-08-21 10:42:14 +0200

    feat(scripts): removed some conditions in install-shell and made it set the default shell to zsh

--- scripts/install-shell.sh
+++ scripts/install-shell.sh
@@ -6,36 +6,34 @@ GIT_DIR="$(git rev-parse --show-toplevel)"
 ZSH_HOME=$HOME/.oh-my-zsh
 ZSH_CUSTOM=$ZSH_HOME/custom
 
-# Install Zsh if needed
-zsh --version >/dev/null 2>&1
-if [ ! $? -eq 0 ]; then
-  echo "Zsh is not installed, installing..."
-  sudo apt install zsh
+echo "Installing zsh (apt)..."
+sudo apt install zsh
+
+# Install .zshrc
+zshrc_src=$GIT_DIR/terminal/.zshrc
+zshrc_dest=$HOME/.zshrc
+echo "Installing .zshrc..."
+echo "$zshrc_src --> $zshrc_dest"
+ln -sf $zshrc_src $zshrc_dest
+
+# Remove oh-my-zsh directory if it already exists
+if [ -d $ZSH_HOME ]; then
+  echo "oh-my-zsh already installed, removing old version..."
+  rm -rf $ZSH_HOME
 fi
 
-# Install oh-my-zsh if needed
-if [ ! -d $ZSH_HOME ]; then
-  zshrc_src=$GIT_DIR/terminal/.zshrc
-  zshrc_dest=$HOME/.zshrc
-  echo "Installing .zshrc..."
-  echo "$zshrc_src --> $zshrc_dest"
-  ln -s $zshrc_src $zshrc_dest
+# Install oh-my-zsh without overwriting our .zshrc,
+# we don't run it immediately because the plugins are not installed yet
+export KEEP_ZSHRC=yes
+export RUNZSH=no
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-  # Install oh-my-zsh without overwriting our .zshrc,
-  # we don't run it immediately because the plugins are not installed yet
-  export KEEP_ZSHRC=yes
-  export RUNZSH=no
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-# Install our custom catppuccin theme if needed
+# Install our custom catppuccin theme
 catppuccin_theme_src=$GIT_DIR/terminal/catppuccin.zsh-theme
 catppuccin_theme_dest=$ZSH_CUSTOM/themes/catppuccin.zsh-theme
-if [ ! -f $catppuccin_theme_dest ]; then
-  echo "Installing catppuccin theme..."
-  echo "$catppuccin_theme_src --> $catppuccin_theme_dest"
-  ln -s $catppuccin_theme_src $catppuccin_theme_dest
-fi
+echo "Installing catppuccin theme..."
+echo "$catppuccin_theme_src --> $catppuccin_theme_dest"
+ln -sf $catppuccin_theme_src $catppuccin_theme_dest
 
 # Install plugins if needed
 plugins=(zsh-autosuggestions zsh-syntax-highlighting)
@@ -46,6 +44,10 @@ for plugin in ${plugins[@]}; do
     git clone https://github.com/zsh-users/$plugin $plugin_path
   fi
 done
+
+# Make zsh the default shell
+echo "Making zsh the default shell..."
+chsh -s $(which zsh)
 
 # Run zsh
 zsh
