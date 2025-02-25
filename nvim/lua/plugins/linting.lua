100e5f6b53b7a1d7645be31b18a475be6bf88f5e
Clément Dreptin <cdreptin@gmail.com>
2025-02-25 11:34:36 +0100

    feat(nvim): stopped trying to load eslint when no eslint config file is found

--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -17,11 +17,40 @@ local lint = {
     local lint = require("lint")
     lint.linters_by_ft = opts.linters_by_ft
 
+    -- Cache for ESLint configuration presence
+    local has_eslint_config_cache = nil
+
+    local function has_eslint_config()
+      if has_eslint_config_cache ~= nil then
+        return has_eslint_config_cache
+      end
+
+      -- ESLint supports other config file names but there aren't recommended or even deprecated
+      -- so I never use them
+      local eslint_config_file_names = {
+        "eslint.config.js",
+        "eslint.config.mjs",
+      }
+
+      -- Check if any of the ESLint config files exist in the workspace
+      for _, config in ipairs(eslint_config_file_names) do
+        if vim.fn.filereadable(config) == 1 then
+          has_eslint_config_cache = true
+          return true
+        end
+      end
+
+      has_eslint_config_cache = false
+      return false
+    end
+
     -- Create the auto command that will run every time one of opts.events event occurs
     vim.api.nvim_create_autocmd(opts.events, {
       group = vim.api.nvim_create_augroup("lint", { clear = true }),
       callback = function()
-        lint.try_lint()
+        if has_eslint_config() then
+          lint.try_lint()
+        end
       end,
     })
   end,
