37324a9ee05283f544288a08f3bafa49380e6c91
Clément Dreptin <cdreptin@gmail.com>
2025-06-11 09:49:39 +0200

    refactor(nvim): more robust way of handling markdown specific options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -27,11 +27,25 @@ opt.wrap = false
 opt.ignorecase = true -- Ignore case when searching
 opt.smartcase = true -- If the search includes a mixed case, assume the search to be case-sensitive
 
--- Enable line wrapping and spell check but only for markdown files
+-- Options specific to markdown files
+local markdown_options = {
+  "wrap",
+  "linebreak",
+  "spell",
+}
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = { "*.md" },
   callback = function()
-    opt.wrap = true
-    opt.spell = true
+    for _, option_name in pairs(markdown_options) do
+      opt[option_name] = true
+    end
+  end,
+})
+vim.api.nvim_create_autocmd("BufLeave", {
+  pattern = { "*.md" },
+  callback = function()
+    for _, option_name in pairs(markdown_options) do
+      opt[option_name] = false
+    end
   end,
 })
