5b51f8f3f310fd3f00a2277d056ea438a8d55d2c
Clément Dreptin <cdreptin@gmail.com>
2025-12-18 11:32:34 +0100

    fix(nvim): more robust way to choose between biome and prettier for formatting

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -7,37 +7,43 @@ local conform = {
   "stevearc/conform.nvim",
   event = { "BufWritePre" }, -- Don't load until a file is written
   cmd = { "ConformInfo" },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { "biome-check", "prettierd", stop_after_first = true },
-      typescript = { "biome-check", "prettierd", stop_after_first = true },
-      javascriptreact = { "biome-check", "prettierd", stop_after_first = true },
-      typescriptreact = { "biome-check", "prettierd", stop_after_first = true },
-      html = { "biome-check", "prettierd", stop_after_first = true },
-      css = { "biome-check", "prettierd", stop_after_first = true },
-      json = { "biome-check", "prettierd", stop_after_first = true },
-      jsonc = { "biome-check", "prettierd", stop_after_first = true },
-      yaml = { "prettierd" },
-      markdown = { "prettierd" },
-    },
-    format_on_save = function(bufnr)
-      -- Don't format if setting is disabled
-      if not vim.g.format_on_save then
-        return
-      end
+  opts = function()
+    -- We use biome if it's available locally, and fallback to prettier otherwise
+    local has_biome_exe = vim.fn.executable("node_modules/.bin/biome")
+    local web_formatter_to_use = has_biome_exe ~= 0 and "biome-check" or "prettierd"
 
-      -- Never run formatter in node_modules
-      local bufname = vim.api.nvim_buf_get_name(bufnr)
-      if bufname:match("/node_modules/") then
-        return
-      end
+    return {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { web_formatter_to_use, stop_after_first = true },
+        typescript = { web_formatter_to_use, stop_after_first = true },
+        javascriptreact = { web_formatter_to_use, stop_after_first = true },
+        typescriptreact = { web_formatter_to_use, stop_after_first = true },
+        html = { web_formatter_to_use, stop_after_first = true },
+        css = { web_formatter_to_use, stop_after_first = true },
+        json = { web_formatter_to_use, stop_after_first = true },
+        jsonc = { web_formatter_to_use, stop_after_first = true },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+      },
+      format_on_save = function(bufnr)
+        -- Don't format if setting is disabled
+        if not vim.g.format_on_save then
+          return
+        end
 
-      return {
-        lsp_format = "fallback", -- Use the lsp formatter if the file type doesn't match any of the above
-      }
-    end,
-  },
+        -- Never run formatter in node_modules
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match("/node_modules/") then
+          return
+        end
+
+        return {
+          lsp_format = "fallback", -- Use the lsp formatter if the file type doesn't match any of the above
+        }
+      end,
+    }
+  end,
 }
 
 -- Command to toggle format on save
